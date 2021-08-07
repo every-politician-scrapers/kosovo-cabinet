3a6b5593f8a037197ff39bfaaae149765962bafb
Tony Bowden <tony@tmtm.com>
2021-08-07 14:19:18 +0300

    Update to every-politician-scraper 0.1.1

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -2,34 +2,25 @@
 # frozen_string_literal: true
 
 require 'every_politician_scraper/scraper_data'
-require 'open-uri/cached'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    field :name do
+  class Member
+    def name
       noko.css('.members-name').text.tidy
     end
 
-    field :position do
+    def position
       noko.css('.members-title').text.tidy
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.map { |member| fragment(member => Member).to_h }
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('.members-list .members-info')
     end
   end
 end
 
-url = 'https://kryeministri.rks-gov.net/en/the-prime-minister/government-cabinet/'
-puts EveryPoliticianScraper::ScraperData.new(url).csv
+file = Pathname.new 'html/official.html'
+puts EveryPoliticianScraper::FileData.new(file).csv
